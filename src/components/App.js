99f72a2f709ee431b030470c8a990232933ef269
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 19:58:12 +0545

    Convert App to a stateful component

--- src/components/App.js
+++ src/components/App.js
@@ -1,33 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import TodoList from './TodoList';
 
-function App() {
-    let items = [
-        {
-            id: 1,
-            text: 'Learn Javascript',
-            completed: false
-        },
-        {
-            id: 2,
-            text: 'Learn React',
-            completed: false
-        },
-        {
-            id: 3,
-            text: 'Build a React App',
-            completed: false
+class App extends Component {
+    constructor() {
+        super();
+        this.state = {
+            items: [
+                {
+                    id: 1,
+                    text: 'Learn Javascript',
+                    completed: false
+                },
+                {
+                    id: 2,
+                    text: 'Learn React',
+                    completed: false
+                },
+                {
+                    id: 3,
+                    text: 'Build a React App',
+                    completed: false
+                }
+            ]
         }
-    ];
-    let title = 'Things to do';
+    }
 
-    return (
-        <div className="container">
-            <div className="row">
-                <TodoList title={title} items={items} />
+    render() {
+        let title = 'Things to do';
+
+        return (
+            <div className="container">
+                <div className="row">
+                    <TodoList title={title} items={this.state.items}/>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
 }
 
 export default App;
