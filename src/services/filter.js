23287d5e44498136e0b89cff061337ad1b8b1c9f
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 21:31:45 +0545

    Add constants

--- src/services/filter.js
+++ src/services/filter.js
@@ -1,10 +1,14 @@
 
+const FILTER_ALL = 'all';
+const FILTER_ACTIVE = 'active';
+const FILTER_COMPLETED = 'completed';
+
 export function applyFilter(list, filter) {
     switch (filter) {
-        case 'completed':
+        case FILTER_COMPLETED:
             return list.filter(item => item.completed === true);
 
-        case 'active':
+        case FILTER_ACTIVE:
             return list.filter(item => item.completed !== true);
 
         default:
