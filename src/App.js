5800996004c11a3eb763ca183ca0ce5b17a3899a
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 18:05:51 +0545

    React Hello World

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
-class App extends Component {
-  render() {
+function App() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+        <h1>Hello World</h1>
     );
-  }
 }
 
 export default App;
