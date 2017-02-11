56ec553cea6c056cf6dc0b4d193264382056f8e4
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 19:49:41 +0545

    Convert InputBox to a stateful component

--- src/components/InputBox.js
+++ src/components/InputBox.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
-export default function InputBox() {
-    return (
-        <input type="text" className="form-control add-todo" placeholder="Add New" />
-    );
+class InputBox extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            value: props.value || ''
+        };
+    }
+
+    handleChange(e) {
+        this.setState({value: e.target.value});
+    }
+
+    render() {
+        return (<input type="text"
+            className="form-control add-todo"
+            value={this.state.value}
+            onChange={this.handleChange.bind(this)}
+            placeholder="Add New"/>);
+    }
 }
+
+export default InputBox;
