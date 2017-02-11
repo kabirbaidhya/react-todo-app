c5cb47cf7c3394e5376d010d7125942bdc4ca8da
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 18:48:44 +0545

    Render the list dynamically using an array of data

--- src/App.js
+++ src/App.js
@@ -1,30 +1,37 @@
 import React from 'react';
 
 function App() {
+    let items = [
+        {
+            text: 'Learn Javascript',
+            completed: false
+        },
+        {
+            text: 'Learn React',
+            completed: false
+        },
+        {
+            text: 'Build a React App',
+            completed: false
+        }
+    ];
+
     return (
         <div className="container">
             <div className="row">
                 <div className="todolist">
                     <h1>Todos</h1>
                     <ul className="list-unstyled">
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Take out the trash</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Buy bread</label>
-                            </div>
-                        </li>
-                        <li className="ui-state-default">
-                            <div className="checkbox">
-                                <label>
-                                    <input type="checkbox" value=""/>Teach penguins to fly</label>
-                            </div>
-                        </li>
+                        {items.map(item => (
+                            <li className="ui-state-default">
+                                <div className="checkbox">
+                                    <label>
+                                        <input type="checkbox" value=""/>
+                                        {item.text}
+                                    </label>
+                                </div>
+                            </li>
+                        ))}
                     </ul>
                 </div>
             </div>
