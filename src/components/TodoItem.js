ebb7cd725e81dd5b91c6ee5088e2a62578e1dfdc
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 23:00:51 +0545

    Display completed items with a line-through

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -4,9 +4,10 @@ import CheckBox from './CheckBox';
 export default function TodoItem(props) {
     const {data, changeStatus} = props;
     const handleChange = (checked) => changeStatus(data.id, checked);
+    const className = 'todo-item ui-state-default ' + (data.completed === true ? 'completed' : 'pending');
 
     return (
-        <li className="ui-state-default">
+        <li className={className}>
             <div className="checkbox">
                 <label>
                     <CheckBox checked={data.completed} onChange={handleChange}/> {data.text}
