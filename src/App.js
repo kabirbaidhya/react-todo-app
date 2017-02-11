4f21d0068d01b35afe445f7ba11eb043c4eb9c8b
Kabir Baidhya <kabirbaidhya@gmail.com>
2017-02-11 18:38:07 +0545

    Add some JSX

--- src/App.js
+++ src/App.js
@@ -2,7 +2,33 @@ import React from 'react';
 
 function App() {
     return (
-        <h1>Hello World</h1>
+        <div className="container">
+            <div className="row">
+                <div className="todolist">
+                    <h1>Todos</h1>
+                    <ul className="list-unstyled">
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Take out the trash</label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Buy bread</label>
+                            </div>
+                        </li>
+                        <li className="ui-state-default">
+                            <div className="checkbox">
+                                <label>
+                                    <input type="checkbox" value=""/>Teach penguins to fly</label>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 }
 
