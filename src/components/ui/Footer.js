74148ab001d44cd7b1998ee4f47e61bd905cad08
Rakhee Singh <rakhisingh2811@gmail.com>
2021-11-27 21:05:40 +0530

    Resolve issue #21 Remaining items are not updating when we complete … (#34)
    * Resolve issue #21 Remaining items are not updating when we complete any task
    
    * PR with suggested changes
    
    Co-authored-by: Rakhee <srakhee@deloitte.com>

--- src/components/ui/Footer.js
+++ src/components/ui/Footer.js
@@ -3,15 +3,14 @@ import Filter from './Filter';
 import ButtonWrapper from './ButtonWrapper';
 
 export default function Footer(props) {
-    const {count, filter, changeFilter} = props;
-
+    const {activeItemCount, filter, changeFilter} = props;
     return (
         <footer className="clearfix">
             <div className="pull-left buttons">
                 <ButtonWrapper {...props}/>
             </div>
             <div className="pull-left">
-                {`${count} items left`}
+                {`${activeItemCount} items left`}
             </div>
             <div className="pull-right">
                 <Filter {...{filter, changeFilter}}/>
--- src/components/ui/TodoList.js
+++ src/components/ui/TodoList.js
@@ -3,12 +3,12 @@ import Info from './Info';
 import Header from './Header';
 import Footer from './Footer';
 import FilteredList from './FilteredList';
-import {applyFilter, search} from '../../services/filter';
+import {applyFilter, search, FILTER_ACTIVE} from '../../services/filter';
 
 export default function TodoList(props) {
     const {list, filter, mode, query} = props.data;
     const {addNew, changeFilter, changeStatus, changeMode, setSearchQuery} = props.actions;
-    const count = list.length;
+    const activeItemCount = applyFilter(list, FILTER_ACTIVE).length;
     const items = search(applyFilter(list, filter), query);
 
     return (
@@ -17,7 +17,7 @@ export default function TodoList(props) {
                 <div className="todolist">
                     <Header {...{addNew, mode, query, setSearchQuery}}/>
                     <FilteredList {...{items, changeStatus}}/>
-                    <Footer {...{count, filter, changeFilter, mode, changeMode}}/>
+                    <Footer {...{activeItemCount, filter, changeFilter, mode, changeMode}}/>
                     <Info {...{mode}}/>
                 </div>
             </div>
