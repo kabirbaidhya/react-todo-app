8949a281742005a22a4faea31e23f8ce6bc61967
Rakhee <srakhee@deloitte.com>
2021-11-27 19:39:07 +0530

    PR with suggested changes

--- src/components/ui/TodoList.js
+++ src/components/ui/TodoList.js
@@ -8,7 +8,7 @@ import {applyFilter, search, FILTER_ACTIVE} from '../../services/filter';
 export default function TodoList(props) {
     const {list, filter, mode, query} = props.data;
     const {addNew, changeFilter, changeStatus, changeMode, setSearchQuery} = props.actions;
-    let activeItemcount = applyFilter(list, FILTER_ACTIVE).length;
+    const activeItemCount = applyFilter(list, FILTER_ACTIVE).length;
     const items = search(applyFilter(list, filter), query);
 
     return (
@@ -17,7 +17,7 @@ export default function TodoList(props) {
                 <div className="todolist">
                     <Header {...{addNew, mode, query, setSearchQuery}}/>
                     <FilteredList {...{items, changeStatus}}/>
-                    <Footer {...{activeItemcount, filter, changeFilter, mode, changeMode}}/>
+                    <Footer {...{activeItemCount, filter, changeFilter, mode, changeMode}}/>
                     <Info {...{mode}}/>
                 </div>
             </div>
