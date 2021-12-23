3984fabfe10559eac34c778724cda987e652375a
Kabir Baidhya <3315763+kabirbaidhya@users.noreply.github.com>
2021-12-23 01:05:44 +0100

    Reformat

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from 'react';
 
 function App() {
   let items = [
@@ -26,14 +26,16 @@ function App() {
         <div className="todolist">
           <h1>{title.toUpperCase()}</h1>
           <ul className="list-group list-group-flush">
-            {items.map(item =>
+            {items.map(item => (
               <li key={item.id} className="list-group-item">
                 <div className="form-check">
                   <input className="form-check-input" type="checkbox" value="" id={`todo-item-check-${item.id}`} />
-                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>{item.text}</label>
+                  <label className="form-check-label" htmlFor={`todo-item-check-${item.id}`}>
+                    {item.text}
+                  </label>
                 </div>
               </li>
-            )}
+            ))}
           </ul>
         </div>
       </div>
--- src/App.test.js
+++ src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders things to do', () => {
   render(<App />);
-  const linkElement = screen.getByText(/things to do/i);
-  expect(linkElement).toBeInTheDocument();
+  const element = screen.getByText(/things to do/i);
+  expect(element).toBeInTheDocument();
 });
