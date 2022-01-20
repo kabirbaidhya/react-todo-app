9b273ebc5ab9d03f981c2e244bfa45362ed528be
kirru396 <reddikiran6@outlook.com>
2022-01-20 15:26:42 +0530

    adding cricket image in header..

--- src/components/ui/Header.js
+++ src/components/ui/Header.js
@@ -1,9 +1,10 @@
 import React from 'react';
-import InputWrapper from './InputWrapper';
+import InputWrapper from './InputWrapper';  
 
 export default function Header(props) {
     return (
         <header>
+            <img src="./assets/images/5492.jpg" alt="Cricket"/>
             <h1>Things To Do</h1>
             <InputWrapper {...props}/>
         </header>
