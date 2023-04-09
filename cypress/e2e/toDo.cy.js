d196d312daab6d1c35cdac1c96191f5cf1cc31f4
tymocoderod <tymocoder@gmail.com>
2023-04-08 23:37:31 -0700

    two more tests added

--- cypress/e2e/toDo.cy.js
+++ cypress/e2e/toDo.cy.js
@@ -1,8 +1,22 @@
 describe('toDo spec', () => {
+  Cypress.config("baseUrl", "http://localhost:3000"); //Cypress.config("baseUrl", "https://petstore.swagger.io/v2");
+  beforeEach(() => {
+    cy.visit('baseUrl')
+  })
   it('TC001 - adding new item', () => {
     const name = "Learn Java"
-    cy.visit('http://localhost:3000')
     cy.get('.form-control').type(name).type("{enter}")
     cy.get('.checkbox').contains(name).should("be.visible")
+  })
+
+  it("TC002 Check checkbox 'Lern Javascript' as done", () => {
+    const name1 = "Learn Javascript"
+    cy.get('.checkbox').contains(name1).click()
+    cy.get('.completed > .checkbox > label').should('exist')
+  })
+
+  it("TC003 Verify complited section is empty", () => {
+    cy.get('a').contains("Completed").click()
+    cy.get('p.alert').should("have.text", "There are no items.")
   })
 })
